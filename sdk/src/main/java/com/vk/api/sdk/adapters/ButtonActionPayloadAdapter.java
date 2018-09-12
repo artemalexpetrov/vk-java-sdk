ffe4c83ce891ce34d5330b3e57095c5ce015ccaa
Andrey Osipov <blaketsk@gmail.com>
2018-09-12 22:51:19 +0700

    added Keyboard to MessagesSendWithUserIdsQuery

--- sdk/src/main/java/com/vk/api/sdk/adapters/ButtonActionPayloadAdapter.java
+++ sdk/src/main/java/com/vk/api/sdk/adapters/ButtonActionPayloadAdapter.java
@@ -1,24 +1,20 @@
 package com.vk.api.sdk.adapters;
 
-import java.io.IOException;
+import java.lang.reflect.Type;
 
 import com.google.gson.Gson;
-import com.google.gson.TypeAdapter;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 import com.vk.api.sdk.objects.messages.ButtonActionPayload;
 
-public class ButtonActionPayloadAdapter extends TypeAdapter<ButtonActionPayload> {
+public class ButtonActionPayloadAdapter implements JsonSerializer<ButtonActionPayload> {
 
     private final Gson gson = new Gson();
 
     @Override
-    public void write(JsonWriter writer, ButtonActionPayload value) throws IOException {
-        writer.jsonValue(gson.toJson(value));
-    }
-
-    @Override
-    public ButtonActionPayload read(JsonReader in) throws IOException {
-        return null;
+    public JsonElement serialize(ButtonActionPayload value, Type type, JsonSerializationContext context) {
+        return new JsonPrimitive(context.serialize(value).toString());
     }
 }
